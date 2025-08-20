5288e65c750cb267969b5a4f73ed4fc03b113861
Anupriya V M <anupriyabvmhss@gmail.com>
2025-08-20 12:15:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to backend!");
+});
+
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+
+  if (username === "admin" && password === "1234") {
+    res.json({ success: true, message: "Login successful!" });
+  } else {
+    res.status(401).json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+
+app.listen(5000, ()=>{
+  console.log("Server running on http://localhost:5000");
+});
